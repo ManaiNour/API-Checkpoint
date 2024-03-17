348e04980002cec11d3d6bcb147d6dff3f2d2cb1
Nour Manai <nourmanai1995@gmail.com>
2024-03-17 01:19:26 +0100

    first commit

---
+++ src/Axios.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+import React from "react";
+import { useState , useEffect } from "react";
+
+
+
+
+
+const Axios=()=>{
+
+    const [data, setData] = useState ([])
+useEffect(() => {
+  axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+    setData(res.data);
+    console.log(res.data);
+  });
+},
+  [])
+  return(
+<div className="div">
+{""}
+{data.map((e)=> (<div className="list">name is : {e.name}  </div> ))}
+  
+  
+</div>
+
+
+  );
+}
+
+
+export default Axios
